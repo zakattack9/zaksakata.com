f46a0fbd1f92c00a150d76b509186c0cc0f5f4a5
zakattack9 <zakattack26111@gmail.com>
2018-07-18 23:55:14 -1000

    testing fullpage.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+    licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+    sectionsColor: ['red', 'green', 'blue'],
+    controlArrows: false,
+		autoScrolling: true,
+    navigation: true,
+    navigationPosition: "right"
+	});
+
+	//methods
+	$.fn.fullpage.setAllowScrolling(false);
+});
