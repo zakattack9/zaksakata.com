f318fb5bcdc85319d82a8f55df9c690d6c1f6f7f
zakattack9 <zakattack26111@gmail.com>
2018-07-21 14:56:08 -1000

    added menu button

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,60 @@ $(document).ready(() => {
   $('#animateName').tilt({
     glare: true,
     maxGlare: 0.5, //determines how bright the glare is
-    scale: 1.05 //scales tilt element
+    scale: 1.05, //scales tilt element
   })
-  
+
 });
+
+$('#menuBtn').on('click', () => {
+  if($('#overlay').height() === 0){
+    $('#overlay')[0].style.height = '400px';
+
+    $('#hr2').css({
+      "width": "30px",
+      "transform": "rotate(90deg)",
+      "margin-bottom": "-4px",
+      "transition": "0.2s",
+    })
+
+    $('#hr3').css({
+      "width": "30px",
+      "transition": "0.2s",
+    })
+
+    $('#hr1').css({
+      "margin-bottom": "-4px",
+      "transition": "0.2s",
+    })
+
+    $('#menuBtn').css({
+      "top": "3.2%",
+      "right": "1.2%",
+      "height": "55px",
+      "transition": "0.2s",
+    })
+  } else {
+    $('#overlay')[0].style.height = '0';
+
+    $('#hr2').css({
+      "width": "40px",
+      "transform": "rotate(0deg)",
+      "margin-bottom": "5px",
+    })
+
+    $('#hr3').css({
+      "width": "60px",
+    })
+
+    $('#hr1').css({
+      "margin-bottom": "",
+    })
+
+    $('#menuBtn').css({
+      "top": "4%",
+      "right": "1%",
+      "height": "40px",
+      "transition": "0.2s",
+    })
+  }
+})
