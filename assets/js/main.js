bd923b59d42aac5eb93c051f2ced7c70bdb67190
zakattack9 <zakattack26111@gmail.com>
2018-07-30 22:15:44 -1000

    working on responsive views

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
   $('#fullpage').fullpage({
     licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
 
-    //autoScrolling: true,
+    //autoScrolling: false,
     navigation: true,
     navigationPosition: "right",
     easingcss3: "ease-in",
@@ -39,7 +39,7 @@ $(document).ready(() => {
 
   AOS.init({
     // Global settings
-    disable: false,
+    disable: 'tablet',
     startEvent: 'DOMContentLoaded',
     initClassName: 'aos-init',
     animatedClassName: 'aos-animate',
@@ -60,71 +60,73 @@ $(document).ready(() => {
     //maxGlare: 0.5, //determines how bright the glare is
     scale: 1.05, //scales tilt element
   })
-
-  //Parallax Scenes for Main Page
-  var scene = document.getElementById('shortBlurb');
-  var parallaxInstance = new Parallax(scene);
-  parallaxInstance.friction(0.9, 0.9);
-  parallaxInstance.scalar(15, 10);
-
-  var scene2 = document.getElementById('shortBlurbSub');
-  var parallaxInstance2 = new Parallax(scene2);
-  parallaxInstance2.friction(0.1, 0.1);
-
-  //Parallax Scenes for About Section
-  var layerScene = document.getElementById('layerScene');
-  var parallaxInstance3 = new Parallax(layerScene);
-  parallaxInstance3.limit(false, 0);
-  parallaxInstance3.invert(false, true);
-  parallaxInstance3.scalar(5, 0);
-  parallaxInstance3.friction(0.3, 0.3);
-
-  var imageScene = document.getElementById('imgParallax');
-  var parallaxInstance4 = new Parallax(imageScene);
-  parallaxInstance4.friction(0.1, 0.1);
-
-  var aboutTextPlx = document.getElementById('aboutTextPlx');
-  var parallaxInstance5 = new Parallax(aboutTextPlx);
-
-  //Parallax Scenes for Skills Section
-  var skillsParallax = document.getElementById('skillsParallax');
-  var parallaxInstance6 = new Parallax(skillsParallax);
-
-  var skillsParallax2 = document.getElementById('skillsParallax2');
-  var parallaxInstance7 = new Parallax(skillsParallax2);
-
-  var skillCardsPlx = document.getElementById('skillCardsPlx');
-  var parallaxInstance8 = new Parallax(skillCardsPlx);
-
-  var newSkillsPlx = document.getElementById('newSkillsPlx');
-  var parallaxInstance9 = new Parallax(newSkillsPlx);
-
-  //Parallax Scenes for Selected Projects Section
-  var projectsTitle = document.getElementById('projectsTitle');
-  var parallaxInstance15 = new Parallax(projectsTitle);
-  parallaxInstance15.invert(false, true);
-
-  var spPlxSubTitle = document.getElementById('spPlxSubTitle');
-  var parallaxInstance15 = new Parallax(spPlxSubTitle);
-
-  //Parallax Scenes for Work Experience Section
-  var logoPlx = document.getElementById('logoPlx');
-  var parallaxInstance10 = new Parallax(logoPlx);
-
-  var wePlxTitle = document.getElementById('wePlxTitle');
-  var parallaxInstance11 = new Parallax(wePlxTitle);
-  parallaxInstance11.invert(false, true);
-
-  var wePlxSubTitle = document.getElementById('wePlxSubTitle');
-  var parallaxInstance12 = new Parallax(wePlxSubTitle);
-  parallaxInstance12.invert(false, true);
-
-  var wePlxDesc1 = document.getElementById('wePlxDesc1');
-  var parallaxInstance13 = new Parallax(wePlxDesc1);
-
-  var wePlxDesc2 = document.getElementById('wePlxDesc2');
-  var parallaxInstance14 = new Parallax(wePlxDesc2);
 });
+
+//Parallax scenes must be out of $(document).ready scop
+//Parallax Scenes for Main Page
+var scene = document.getElementById('shortBlurb');
+var parallaxInstance = new Parallax(scene);
+parallaxInstance.friction(0.9, 0.9);
+parallaxInstance.scalar(15, 10);
+
+var scene2 = document.getElementById('shortBlurbSub');
+var parallaxInstance2 = new Parallax(scene2);
+parallaxInstance2.friction(0.1, 0.1);
+
+//Parallax Scenes for About Section
+var layerScene = document.getElementById('layerScene');
+var parallaxInstance3 = new Parallax(layerScene);
+parallaxInstance3.limit(false, 0);
+parallaxInstance3.invert(false, true);
+parallaxInstance3.scalar(5, 0);
+parallaxInstance3.friction(0.3, 0.3);
+
+var imageScene = document.getElementById('imgParallax');
+var parallaxInstance4 = new Parallax(imageScene);
+parallaxInstance4.friction(0.1, 0.1);
+
+var aboutTextPlx = document.getElementById('aboutTextPlx');
+var parallaxInstance5 = new Parallax(aboutTextPlx);
+
+//Parallax Scenes for Skills Section
+var skillsParallax = document.getElementById('skillsParallax');
+var parallaxInstance6 = new Parallax(skillsParallax);
+
+var skillsParallax2 = document.getElementById('skillsParallax2');
+var parallaxInstance7 = new Parallax(skillsParallax2);
+
+var skillCardsPlx = document.getElementById('skillCardsPlx');
+var parallaxInstance8 = new Parallax(skillCardsPlx);
+
+var newSkillsPlx = document.getElementById('newSkillsPlx');
+var parallaxInstance9 = new Parallax(newSkillsPlx);
+
+//Parallax Scenes for Selected Projects Section
+var projectsTitle = document.getElementById('projectsTitle');
+var parallaxInstance15 = new Parallax(projectsTitle);
+parallaxInstance15.invert(false, true);
+
+var spPlxSubTitle = document.getElementById('spPlxSubTitle');
+var parallaxInstance16 = new Parallax(spPlxSubTitle);
+
+//Parallax Scenes for Work Experience Section
+var logoPlx = document.getElementById('logoPlx');
+var parallaxInstance10 = new Parallax(logoPlx);
+
+var wePlxTitle = document.getElementById('wePlxTitle');
+var parallaxInstance11 = new Parallax(wePlxTitle);
+parallaxInstance11.invert(false, true);
+
+var wePlxSubTitle = document.getElementById('wePlxSubTitle');
+var parallaxInstance12 = new Parallax(wePlxSubTitle);
+parallaxInstance12.invert(false, true);
+
+var wePlxDesc1 = document.getElementById('wePlxDesc1');
+var parallaxInstance13 = new Parallax(wePlxDesc1);
+
+var wePlxDesc2 = document.getElementById('wePlxDesc2');
+var parallaxInstance14 = new Parallax(wePlxDesc2);
+
 
 //opens and closes overlay
 function closeOverlay() {
@@ -247,8 +249,8 @@ function openPrjOverlay(projectNum) {
   $('#fp-nav')[0].style.display = 'none';
 
   var selectedPrj = projects[projectNum - 1]; //subtract 1 since the parameter being passed in is not based off zero index scale
-  var prjTemplate = 
-  `<div class="row mt-md-5 justify-content-md-center">
+  var prjTemplate =
+    `<div class="row mt-md-5 justify-content-md-center">
     <div class="prjID">Project 0${selectedPrj.id}</div>
     <div class="col-md-11 prjTitle title">${selectedPrj.title}</div>
     <div class="col-md-11 subTitle">${selectedPrj.subTitle}</div>
@@ -307,7 +309,7 @@ $('#send').on('click', () => { //validates all field inputs are filled and inclu
     $('#subjectInp').addClass('error');
 
     formValid = false;
-    
+
   } if ($('#msgInp').val() === '') {
     $('#msgInp').addClass('error');
     $('#msgInp')[0].style.backgroundColor = "#ff6d6d";
@@ -323,26 +325,24 @@ $('#send').on('click', () => { //validates all field inputs are filled and inclu
       contentType: "application/json; charset=utf-8",
       dataType: 'JSON',
       data: JSON.stringify({
-        "name" : $('#nameInp').val(),
-        "email" : $('#emailInp').val(),
-        "subject" : $('#subjectInp').val(),
-        "message" : $('#msgInp').val()
+        "name": $('#nameInp').val(),
+        "email": $('#emailInp').val(),
+        "subject": $('#subjectInp').val(),
+        "message": $('#msgInp').val()
       })
     })
-    .done((response) => {
-      $('#hideSubmit').fadeTo(500, 0, () => {
-        $('#showSubmit')[0].style.zIndex = "2";
+      .done((response) => {
+        $('#hideSubmit').fadeTo(500, 0, () => {
+          $('#showSubmit')[0].style.zIndex = "2";
           $('#showSubmit')[0].style.opacity = "1";
-      });
-    })
-    .fail((err) => {
-      $('#hideSubmit').fadeTo(500, 0, () => {
-        $('#showFail')[0].style.zIndex = "2";
+        });
+      })
+      .fail((err) => { //show error page
+        $('#hideSubmit').fadeTo(500, 0, () => {
+          $('#showFail')[0].style.zIndex = "2";
           $('#showFail')[0].style.opacity = "1";
-      });
-    })
-
-    
+        });
+      })
 
   }
 
---
+++ assets/js/responsive.js
@@ -0,0 +1,64 @@
+//JS media queries for disabling certain libraries
+$(window).resize(() => {
+  /* Smartphone Portrait Mode */
+  if (window.matchMedia("(min-width: 0px)").matches) {
+    fullpage_api.setResponsive(true);
+
+    parallaxInstance.disable();
+    parallaxInstance2.disable();
+    parallaxInstance3.disable();
+    parallaxInstance4.disable();
+    parallaxInstance5.disable();
+    parallaxInstance6.disable();
+    parallaxInstance7.disable();
+    parallaxInstance8.disable();
+    parallaxInstance9.disable();
+    parallaxInstance10.disable();
+    parallaxInstance11.disable();
+    parallaxInstance12.disable();
+    parallaxInstance13.disable();
+    parallaxInstance14.disable();
+    parallaxInstance15.disable();
+    parallaxInstance16.disable();
+    
+  }
+
+  /* Small devices (landscape phones, 576px and up) */
+  if (window.matchMedia("(min-width: 576px)").matches) {
+
+  }
+
+  /* Medium devices (tablets, 768px and up) */
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    
+  }
+  
+  /* Large devices (desktops, 992px and up) */
+  if (window.matchMedia("(min-width: 992px)").matches) {
+    console.log('test');
+    fullpage_api.setResponsive(false);
+
+    parallaxInstance.enable();
+    parallaxInstance2.enable();
+    parallaxInstance3.enable();
+    parallaxInstance4.enable();
+    parallaxInstance5.enable();
+    parallaxInstance6.enable();
+    parallaxInstance7.enable();
+    parallaxInstance8.enable();
+    parallaxInstance9.enable();
+    parallaxInstance10.enable();
+    parallaxInstance11.enable();
+    parallaxInstance12.enable();
+    parallaxInstance13.enable();
+    parallaxInstance14.enable();
+    parallaxInstance15.enable();
+    parallaxInstance16.enable();
+
+  }
+
+  /* Extra large devices (large desktops, 1200px and up) */
+  if (window.matchMedia("(min-width: 1200px)").matches) {
+
+  }
+});
