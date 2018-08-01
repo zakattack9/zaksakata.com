a07a28c88d625854c64e04b4eb7b6fd1d7ed7077
zakattack9 <zakattack26111@gmail.com>
2018-07-31 22:03:13 -1000

    fixing responsiveness

--- assets/js/projectData.js
+++ assets/js/projectData.js
@@ -6,7 +6,7 @@ const projects = [
     subTitle: "Highschool Capstone Project",
     role: "Project Manager",
     techStack: "AWS, JS, HTML, CSS",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et necessitatibus labore distinctio velit tempore temporibus obcaecati,ullam pariatur sunt. Molestiae dolorum corporis fuga quas, aliquam eos incidunt perferendis odit nesciunt!Molestiae dolorum corporis fuga quas, aliquam eos incidunt perferendis odit nesciunt!",
+    description: "Lanakila Meals on Wheels is an elderly meal service that asked us to create an application which would provide them with a better system of communication. With our app, Lanakila can create groups and add subscribers to these groups; they can then create messages that are broadcasted to those groups through various mediums simultaneously with the push of a button.",
     imageUrls: [
       "./assets/img/projects/lanakila1.png",
       "./assets/img/projects/lanakila2.png",
@@ -21,7 +21,7 @@ const projects = [
     subTitle: "Website Redesign",
     role: "UI / UX Designer",
     techStack: "Wix",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et necessitatibus labore distinctio velit tempore temporibus obcaecati,ullam pariatur sunt. Molestiae dolorum corporis fuga quas, aliquam eos incidunt perferendis odit nesciunt!Molestiae dolorum corporis fuga quas, aliquam eos incidunt perferendis odit nesciunt!",
+    description: "I was asked by my former highschool to help design their website intended for recieving funds from benefactors. The main interfaces I helped design were the home, donation, and alumni stories page. I kept a bright and simple interface that made for easy navigation while really highlighting student projects and the highschool's mission.",
     imageUrls: [
       "./assets/img/projects/whs1.png",
       "./assets/img/projects/whs2.png",
@@ -36,15 +36,15 @@ const projects = [
     subTitle: "Hawaii Annual Code Challenge 1st Place Project In Highschool Division",
     role: "Lead Developer",
     techStack: "AWS, HTML, CSS, JS",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et necessitatibus labore distinctio velit tempore temporibus obcaecati,ullam pariatur sunt. Molestiae dolorum corporis fuga quas, aliquam eos incidunt perferendis odit nesciunt!Molestiae dolorum corporis fuga quas, aliquam eos incidunt perferendis odit nesciunt!",
+    description: "My team and I created a chatbot with a custom AI that would assist the Hawaii's Office of Elections in scheduling volunteers during voting months; our application idea took a more modern/interactive approach to scheduling volunteers as opposed to traditional methods of registration such as plain forms or calendars/phone calls.",
     linkToArticle: "", //link to article on win
     imageUrls: [
       "./assets/img/projects/daizzy1.png",
       "./assets/img/projects/daizzy2.png",
       "./assets/img/projects/daizzy3.png",
     ],
-    websiteLink: "",
-    githubLink: ""
+    websiteLink: "https://zakattack9.github.io/DAIZZY/",
+    githubLink: "https://github.com/zakattack9/DAIZZY"
   },
   {
     id: 4,
@@ -52,15 +52,15 @@ const projects = [
     subTitle: "Hawaii Annual Code Challenge For Education 1st Place Project",
     role: "Lead Developer",
     techStack: "HTML, CSS, JS",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et necessitatibus labore distinctio velit tempore temporibus obcaecati,ullam pariatur sunt. Molestiae dolorum corporis fuga quas, aliquam eos incidunt perferendis odit nesciunt!Molestiae dolorum corporis fuga quas, aliquam eos incidunt perferendis odit nesciunt!",
+    description: "The Defenders of the Bay app, integrated Google Maps API to map out algae spots on a beach where volunteers could then sign up for a plot and reserve that area to clean. In addition, our app used data visualization with a custom forum to help build a community amongst the spread of invasive algae across beaches.",
     linkToArticle: "", //link to article on win
     imageUrls: [
       "./assets/img/projects/dotb1.png",
       "./assets/img/projects/dotb2.png",
       "./assets/img/projects/dotb3.png",
     ],
-    websiteLink: "",
-    githubLink: ""
+    websiteLink: "https://zakattack9.github.io/HACC4Edu/",
+    githubLink: "https://github.com/zakattack9/HACC4Edu"
   },
   {
     id: 5,
@@ -68,14 +68,14 @@ const projects = [
     subTitle: "AT&T 2018 Hackathon 2nd Place Project In The Gaming And Entertainment Category",
     role: "Lead Developer",
     techStack: "AWS, HTML, CSS, JS, Spotify API",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et necessitatibus labore distinctio velit tempore temporibus obcaecati,ullam pariatur sunt. Molestiae dolorum corporis fuga quas, aliquam eos incidunt perferendis odit nesciunt!Molestiae dolorum corporis fuga quas, aliquam eos incidunt perferendis odit nesciunt!",
+    description: "The application chromaVibez, took a unique twist on music and color, with the driving thought of what color would sound like? With chromaVibez, users can upload any picture which is then processed through our algorithm that returns a Spotify playlist based off the mood and colors of the picture uploaded.",
     linkToArticle: "", //link to article on win
     imageUrls: [
       "./assets/img/projects/chroma1.png",
       "./assets/img/projects/chroma2.png",
       "./assets/img/projects/chroma3.png",
     ],
-    websiteLink: "",
-    githubLink: ""
+    websiteLink: "https://zakattack9.github.io/chromaVibez/index.html",
+    githubLink: "https://github.com/zakattack9/chromaVibez"
   }
 ];
