82415f0440dd14f95d6bd9bdf41db6a232fc148d
zakattack9 <zakattack26111@gmail.com>
2018-08-23 01:54:41 -0700

    finished overlay

--- photography/js/main.js
+++ photography/js/main.js
@@ -1,6 +1,6 @@
 AOS.init({
   // Global settings
-  disable: enableAOS(), //does not disable on window resize
+  disable: false, //does not disable on window resize
   startEvent: 'DOMContentLoaded',
   initClassName: 'aos-init',
   animatedClassName: 'aos-animate',
@@ -15,3 +15,43 @@ AOS.init({
   mirror: false,
   anchorPlacement: 'top-center',
 });
+
+let overlayOpen = false;
+$('#sideMenu').on('click', () => {
+  if (overlayOpen) {
+    overlayOpen = false;
+
+    $('#overlay')[0].style.right = "-30%";
+    $('#homeText')[0].style.left = "0";
+    $('#socialLinks')[0].style.left = "0";
+
+    $('#line1')[0].style.marginBottom = "13px";
+    $('#line2')[0].style.marginBottom = "13px";
+    $('#line3')[0].style.opacity = "1";
+
+    $('#line1')[0].style.transform = "rotate(0deg)";
+    $('#line2')[0].style.transform = "rotate(0deg)";
+
+    $('#sideMenu').removeClass('overlayOpen');
+    $('#sideMenu').addClass('overlayClose');
+
+  } else {
+    overlayOpen = true;
+
+    $('#overlay')[0].style.right = "0";
+    $('#homeText')[0].style.left = "-15%";
+    $('#socialLinks')[0].style.left = "-15%";
+
+    $('#line1')[0].style.marginBottom = "-3px";
+    $('#line2')[0].style.marginBottom = "-3px";
+    $('#line3')[0].style.opacity = "0";
+
+    $('#line1')[0].style.transform = "rotate(45deg)";
+    $('#line2')[0].style.transform = "rotate(-45deg)";
+
+    $('#sideMenu').removeClass('overlayClose');
+    $('#sideMenu').addClass('overlayOpen');
+  }
+
+  
+})
