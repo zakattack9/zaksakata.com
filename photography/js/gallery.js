dad71459bc0004e3b51016628bc94209e292c374
zakattack9 <zakattack26111@gmail.com>
2018-08-24 13:43:55 -0700

    added functionality to swap out pictures

--- photography/js/gallery.js
+++ photography/js/gallery.js
@@ -16,6 +16,9 @@ AOS.init({
   anchorPlacement: 'top-center',
 });
 
+// Initialize Masonry
+
+
 let overlayOpen = false;
 $('#sideMenu').on('click', () => {
   if (overlayOpen) {
@@ -50,3 +53,66 @@ $('#sideMenu').on('click', () => {
   }
   
 })
+
+$('#gallerySub').on('click', () => {
+  location.reload();
+})
+
+function shuffle (array) { //shuggle images
+  let i = 0;
+  let j = 0;
+  let temp = null;
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+}
+
+//console.log(PHOTO_DATA);
+function showPictures (category, folder) {
+  $('#galleryMain').empty();
+  let clonedAnim = $('#loadingAnim').clone()
+  $('#galleryMain').append(clonedAnim);
+  clonedAnim[0].style.display = "block";
+
+  let appendImgs = ``;
+  shuffle(PHOTO_DATA[category]);
+  PHOTO_DATA[category].map((currVal) => {
+    //console.log(currVal);
+    appendImgs += `
+    <div class="col-md-4 pr-md-5 pt-md-5 pb-3 grid-item">
+      <img class="img-fluid" src="../img/${folder}/${currVal.img}">
+    </div>
+    `;
+  })
+  
+  let photoLayout =
+  `
+  <div class="row pt-2 pb-5">
+    <div class="col-lg-12">
+      <div class="gallery-wrapper clearfix">
+        <div class="col-md-4 grid-sizer"></div>
+
+        ${appendImgs}
+      </div>
+    </div>
+  </div>
+  `
+
+  $('#galleryMain').append(photoLayout);
+  $('#galleryMain .row').hide().delay(1500).fadeIn(1500);
+
+  setTimeout(() => {
+    $('.gallery-wrapper').masonry({
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      percentPosition: true,
+      transitionDuration: 0,
+    });
+    clonedAnim[0].style.display = "none";
+  }, 1500); //need to change timeout for more pictures
+
+}
---
+++ photography/js/photoData.js
@@ -0,0 +1,355 @@
+const PHOTO_DATA = [
+  [
+    //"japan",
+    {
+      id: 1,
+      img: "1.jpg",
+      location: "Nezu Shrine",
+      date: "06•2018"
+    },
+    {
+      id: 2,
+      img: "2.jpg",
+      location: "Nezu Shrine",
+      date: "06•2018"
+    },
+    {
+      id: 3,
+      img: "3.jpg",
+      location: "Nezu Shrine",
+      date: "06•2018"
+    },
+    {
+      id: 4,
+      img: "4.jpg",
+      location: "Nezu Shrine",
+      date: "06•2018"
+    },
+    {
+      id: 5,
+      img: "5.jpg",
+      location: "Nezu Shrine",
+      date: "06•2018"
+    },
+    {
+      id: 6,
+      img: "6.jpg",
+      location: "Nezu Shrine",
+      date: "06•2018"
+    },
+    {
+      id: 7,
+      img: "7.jpg",
+      location: "Ueno",
+      date: "06•2018"
+    },
+    {
+      id: 8,
+      img: "8.jpg",
+      location: "Ueno",
+      date: "06•2018"
+    },
+    {
+      id: 9,
+      img: "9.jpg",
+      location: "Akihabara",
+      date: "06•2018"
+    },
+    {
+      id: 10,
+      img: "10.jpg",
+      location: "Akihabara",
+      date: "06•2018"
+    },
+    {
+      id: 11,
+      img: "11.jpg",
+      location: "Shimbashi",
+      date: "06•2018"
+    },
+    {
+      id: 12,
+      img: "12.jpg",
+      location: "Shimbashi",
+      date: "06•2018"
+    },
+    {
+      id: 13,
+      img: "13.jpg",
+      location: "Odaiba",
+      date: "06•2018"
+    },
+    {
+      id: 14,
+      img: "14.jpg",
+      location: "Tokyu Plaza Omotesando Harajuku",
+      date: "06•2018"
+    },
+    {
+      id: 15,
+      img: "15.jpg",
+      location: "Shibuya Crossing",
+      date: "06•2018"
+    },
+    {
+      id: 16,
+      img: "16.jpg",
+      location: "Shibuya",
+      date: "06•2018"
+    },
+    {
+      id: 17,
+      img: "17.jpg",
+      location: "Shibuya",
+      date: "06•2018"
+    },
+    {
+      id: 18,
+      img: "18.jpg",
+      location: "Tsukiji",
+      date: "06•2018"
+    },
+    {
+      id: 19,
+      img: "19.jpg",
+      location: "Tsukiji",
+      date: "06•2018"
+    },
+    {
+      id: 20,
+      img: "20.jpg",
+      location: "Tsukiji",
+      date: "06•2018"
+    },
+    {
+      id: 21,
+      img: "21.jpg",
+      location: "Tsukiji",
+      date: "06•2018"
+    },
+    {
+      id: 22,
+      img: "22.jpg",
+      location: "Ginza",
+      date: "06•2018"
+    },
+    {
+      id: 23,
+      img: "23.jpg",
+      location: "Ginza",
+      date: "06•2018"
+    },
+    {
+      id: 24,
+      img: "24.jpg",
+      location: "Mori Tower",
+      date: "06•2018"
+    },
+    {
+      id: 25,
+      img: "25.jpg",
+      location: "Mori Tower",
+      date: "06•2018"
+    },
+    {
+      id: 26,
+      img: "26.jpg",
+      location: "Mori Tower",
+      date: "06•2018"
+    },
+    {
+      id: 27,
+      img: "27.jpg",
+      location: "Mori Tower",
+      date: "06•2018"
+    },
+    {
+      id: 28,
+      img: "28.jpg",
+      location: "Mori Tower",
+      date: "06•2018"
+    },
+    {
+      id: 29,
+      img: "29.jpg",
+      location: "Mori Tower",
+      date: "06•2018"
+    },
+    {
+      id: 30,
+      img: "30.jpg",
+      location: "Tokyo Tower",
+      date: "06•2018"
+    },
+    {
+      id: 31,
+      img: "31.jpg",
+      location: "Mori Tower",
+      date: "06•2018"
+    },
+    {
+      id: 32,
+      img: "32.jpg",
+      location: "Mori Tower",
+      date: "06•2018"
+    },
+    {
+      id: 33,
+      img: "33.jpg",
+      location: "Tokyo Tower",
+      date: "06•2018"
+    },
+    {
+      id: 34,
+      img: "34.jpg",
+      location: "Tokyo Tower",
+      date: "06•2018"
+    },
+    {
+      id: 35,
+      img: "35.jpg",
+      location: "Shinjuku",
+      date: "06•2018"
+    },
+    {
+      id: 36,
+      img: "36.jpg",
+      location: "Shinjuku",
+      date: "06•2018"
+    },
+    {
+      id: 37,
+      img: "37.jpg",
+      location: "Shinjuku",
+      date: "06•2018"
+    },
+    {
+      id: 38,
+      img: "38.jpg",
+      location: "Shinjuku",
+      date: "06•2018"
+    },
+    {
+      id: 39,
+      img: "39.jpg",
+      location: "Shinjuku",
+      date: "06•2018"
+    },
+    {
+      id: 40,
+      img: "40.jpg",
+      location: "Shinjuku",
+      date: "06•2018"
+    },
+    {
+      id: 41,
+      img: "41.jpg",
+      location: "Shinjuku",
+      date: "06•2018"
+    },
+    {
+      id: 42,
+      img: "42.jpg",
+      location: "Shinjuku",
+      date: "06•2018"
+    },
+    {
+      id: 43,
+      img: "43.jpg",
+      location: "JR Line",
+      date: "06•2018"
+    },
+    {
+      id: 44,
+      img: "44.jpg",
+      location: "Yokohama",
+      date: "06•2018"
+    },
+    {
+      id: 45,
+      img: "45.jpg",
+      location: "Yokohama",
+      date: "06•2018"
+    },
+    {
+      id: 46,
+      img: "46.jpg",
+      location: "Senso-Ji Temple",
+      date: "06•2018"
+    },
+    {
+      id: 47,
+      img: "47.jpg",
+      location: "Senso-Ji Temple",
+      date: "06•2018"
+    },
+    {
+      id: 48,
+      img: "48.jpg",
+      location: "Senso-Ji Temple",
+      date: "06•2018"
+    },
+    {
+      id: 49,
+      img: "49.jpg",
+      location: "Nezu Shrine",
+      date: "06•2018"
+    },
+  ],
+
+  [
+    //"arizona",
+    {
+      id: 1,
+      img: "AZ1.jpg",
+      location: "Sedona",
+      date: "07•2018"
+    },
+    {
+      id: 2,
+      img: "AZ2.jpg",
+      location: "Sedona",
+      date: "07•2018"
+    },
+    {
+      id: 3,
+      img: "AZ3.jpg",
+      location: "Bell Rock",
+      date: "07•2018"
+    },
+    {
+      id: 4,
+      img: "AZ4.jpg",
+      location: "Sedona",
+      date: "07•2018"
+    },
+    {
+      id: 5,
+      img: "AZ5.jpg",
+      location: "Sedona",
+      date: "07•2018"
+    },
+    {
+      id: 6,
+      img: "AZ6.jpg",
+      location: "Chapel Of The Holy Cross",
+      date: "07•2018"
+    },
+    {
+      id: 7,
+      img: "AZ7.jpg",
+      location: "Chapel Of The Holy Cross",
+      date: "07•2018"
+    },
+    {
+      id: 8,
+      img: "AZ8.jpg",
+      location: "Chapel Of The Holy Cross",
+      date: "07•2018"
+    },
+  ],
+
+  [
+    //"smoke bombs",
+  ],
+]
