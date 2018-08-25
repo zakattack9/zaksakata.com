fbc2b72a49257fd4945d388ecf676f2d29beafc2
zakattack9 <zakattack26111@gmail.com>
2018-08-24 18:04:02 -0700

    added smoke bombs

--- photography/js/photoData.js
+++ photography/js/photoData.js
@@ -166,7 +166,7 @@ const PHOTO_DATA = [
     {
       id: 28,
       img: "28.jpg",
-      location: "Mori Tower",
+      location: "Senso-Ji Temple",
       date: "06•2018"
     },
     {
@@ -295,6 +295,55 @@ const PHOTO_DATA = [
       location: "Nezu Shrine",
       date: "06•2018"
     },
+    {
+      id: 50,
+      img: "50.jpg",
+      location: "Ueno",
+      date: "06•2018"
+    },
+    {
+      id: 51,
+      img: "51.jpg",
+      location: "Ueno",
+      date: "06•2018"
+    },
+    {
+      id: 52,
+      img: "52.jpg",
+      location: "Nezu Shrine",
+      date: "06•2018"
+    },
+    {
+      id: 53,
+      img: "53.jpg",
+      location: "Nezu Shrine",
+      date: "06•2018"
+    },
+    {
+      id: 54,
+      img: "54.jpg",
+      location: "Diver City",
+      date: "06•2018"
+    },
+    {
+      id: 55,
+      img: "55.jpg",
+      location: "Harajuku",
+      date: "06•2018"
+    },
+    {
+      id: 56,
+      img: "56.jpg",
+      location: "Golden Gai",
+      date: "06•2018"
+    },
+    {
+      id: 57,
+      img: "57.jpg",
+      location: "Senso-Ji Temple",
+      date: "06•2018"
+    },
+
   ],
 
   [
@@ -351,5 +400,299 @@ const PHOTO_DATA = [
 
   [
     //"smoke bombs",
+    {
+      id: 1,
+      img: "SB1.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 2,
+      img: "SB2.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 3,
+      img: "SB3.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 4,
+      img: "SB4.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 5,
+      img: "SB5.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 6,
+      img: "SB6.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 7,
+      img: "SB7.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 8,
+      img: "SB8.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 9,
+      img: "SB9.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 10,
+      img: "SB10.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 11,
+      img: "SB11.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 12,
+      img: "SB12.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 13,
+      img: "SB13.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 14,
+      img: "SB14.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 15,
+      img: "SB15.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 16,
+      img: "SB16.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 17,
+      img: "SB17.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 18,
+      img: "SB18.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 19,
+      img: "SB19.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 20,
+      img: "SB20.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 21,
+      img: "SB21.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 22,
+      img: "SB22.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 23,
+      img: "SB23.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 24,
+      img: "SB24.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 25,
+      img: "SB25.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 26,
+      img: "SB26.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 27,
+      img: "SB27.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 28,
+      img: "SB28.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 29,
+      img: "SB29.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 30,
+      img: "SB30.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 31,
+      img: "SB31.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 32,
+      img: "SB32.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 32,
+      img: "SB32.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 33,
+      img: "SB33.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 34,
+      img: "SB34.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 35,
+      img: "SB35.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 36,
+      img: "SB36.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 37,
+      img: "SB37.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 38,
+      img: "SB38.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 39,
+      img: "SB39.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 40,
+      img: "SB40.jpg",
+      location: "Kunia, Hawaii",
+      date: "01•2018"
+    },
+    {
+      id: 41,
+      img: "SB41.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 42,
+      img: "SB42.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 43,
+      img: "SB43.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 44,
+      img: "SB44.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 45,
+      img: "SB45.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
+    {
+      id: 46,
+      img: "SB46.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 47,
+      img: "SB47.jpg",
+      location: "Kunia, Hawaii",
+      date: "10•2017"
+    },
+    {
+      id: 48,
+      img: "SB48.jpg",
+      location: "Kunia, Hawaii",
+      date: "11•2017"
+    },
   ],
 ]
