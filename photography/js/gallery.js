45997b79ce3eb6ed5be489f83040774e859075ad
zakattack9 <zakattack26111@gmail.com>
2018-08-27 01:58:07 -0700

    added about me page

--- photography/js/gallery.js
+++ photography/js/gallery.js
@@ -21,41 +21,6 @@ history.pushState({ selectedFolder: 'gallery' }, 'newtitle', `/photography/galle
 
 $('#imgPopup')[0].style.paddingTop = $('#galleryHeader').height() + 'px';
 
-let overlayOpen = false;
-$('#sideMenu').on('click', () => {
-  if (overlayOpen) {
-    overlayOpen = false;
-
-    $('#overlay')[0].style.right = "-30%";
-
-    $('#line1')[0].style.marginBottom = "13px";
-    $('#line2')[0].style.marginBottom = "13px";
-    $('#line3')[0].style.opacity = "1";
-
-    $('#line1')[0].style.transform = "rotate(0deg)";
-    $('#line2')[0].style.transform = "rotate(0deg)";
-
-    $('#sideMenu').removeClass('overlayOpen');
-    $('#sideMenu').addClass('overlayClose');
-
-  } else {
-    overlayOpen = true;
-
-    $('#overlay')[0].style.right = "0";
-
-    $('#line1')[0].style.marginBottom = "-3px";
-    $('#line2')[0].style.marginBottom = "-3px";
-    $('#line3')[0].style.opacity = "0";
-
-    $('#line1')[0].style.transform = "rotate(45deg)";
-    $('#line2')[0].style.transform = "rotate(-45deg)";
-
-    $('#sideMenu').removeClass('overlayClose');
-    $('#sideMenu').addClass('overlayOpen');
-  }
-
-});
-
 $('#gallerySub').on('click', () => {
   window.location = "../gallery/";
 });
