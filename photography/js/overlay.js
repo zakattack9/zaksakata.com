e18aba99ea37d79e6755eff3c92884a923231d80
zakattack9 <zakattack26111@gmail.com>
2018-08-28 18:31:27 -0700

    added darken on overlay open

--- photography/js/overlay.js
+++ photography/js/overlay.js
@@ -1,22 +1,36 @@
+function closeOverlay() {
+  overlayOpen = false;
+
+  $('#darken').fadeTo(400, 0, () => {
+    $('#darken')[0].style.display = "none";
+  });
+
+  $('#overlay')[0].style.right = "-30%";
+
+  $('#line1')[0].style.marginBottom = "13px";
+  $('#line2')[0].style.marginBottom = "13px";
+  $('#line3')[0].style.opacity = "1";
+
+  $('#line1')[0].style.transform = "rotate(0deg)";
+  $('#line2')[0].style.transform = "rotate(0deg)";
+
+  $('#sideMenu').removeClass('overlayOpen');
+  $('#sideMenu').addClass('overlayClose');
+}
+
+$('#darken').on('click', () => { //closes overlay if user clicks outside of overlay
+  closeOverlay();
+})
+
 let overlayOpen = false;
 $('#sideMenu').on('click', () => {
   if (overlayOpen) {
-    overlayOpen = false;
-
-    $('#overlay')[0].style.right = "-30%";
-
-    $('#line1')[0].style.marginBottom = "13px";
-    $('#line2')[0].style.marginBottom = "13px";
-    $('#line3')[0].style.opacity = "1";
-
-    $('#line1')[0].style.transform = "rotate(0deg)";
-    $('#line2')[0].style.transform = "rotate(0deg)";
-
-    $('#sideMenu').removeClass('overlayOpen');
-    $('#sideMenu').addClass('overlayClose');
-
+    closeOverlay();
   } else {
     overlayOpen = true;
+
+    $('#darken')[0].style.display = "block";
+    $('#darken').fadeTo(400, 0.7);
 
     $('#overlay')[0].style.right = "0";
 
